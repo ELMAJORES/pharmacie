33803fdbc17260c4866fb78bb42bfd0dd484666a
ELMAJORES <tlemcaniabdelhak2008@gmail.com>
2023-04-08 13:47:21 +0200

    import some files from my last project

---
+++ connexion-client.py
@@ -0,0 +1,19 @@
+import csv
+class connexion_client(object):
+	def __init__(self, nom = "xxx",prenom = "xxx"):
+		self.nom = nom
+		self.prenom = prenom
+
+	def connect(self):
+		reader = csv.DictReader(open(r"C:\Users\tlemc\OneDrive\Bureau\pharma/data2.csv"))
+		for raw in reader:
+			if raw["nom"] == self.nom and raw["prenom"] == self.prenom:
+				return True
+
+		return False
+
+
+
+connexion = connexion_client("tlemcani","abdelhak")
+
+print(connexion.connect())
---
+++ produits.py
@@ -0,0 +1,17 @@
+class Produit:
+    def __init__(self, nom_generique, produit_type, prix_achat, prix_vente, date_peremption):
+        self.nom_generique = nom_generique
+        self.produit_type = produit_type
+        self.prix_achat = prix_achat
+        self.prix_vente = prix_vente
+        self.date_peremption = date_peremption
+        self.pharmacies = []
+
+    def add_pharmacie(self, pharmacie):
+        self.pharmacies.append(pharmacie)
+
+    def remove_pharmacie(self, pharmacie):
+        self.pharmacies.remove(pharmacie)
+
+    def __str__(self):
+        return f"{self.nom_generique}, {self.produit_type}, {self.prix_achat}, {self.prix_vente}, {self.date_peremption}"
