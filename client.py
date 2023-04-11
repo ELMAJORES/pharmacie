8774af277423d18cc171c51556e6b4ec26110c5d
ELMAJORES <tlemcaniabdelhak2008@gmail.com>
2023-04-11 11:11:01 +0200

    branch : feature-client amelioration classe client

--- client.py
+++ client.py
@@ -1,8 +1,9 @@
 import csv
 class Client(object):
-	def __init__(self, nom = "xxx",prenom = "xxx"):
+	def __init__(self, nom = "xxx",prenom = "xxx", age = "xx"):
 		self.nom = nom
 		self.prenom = prenom
+		self.age = age
 
 	def connect(self, path):
 		reader = csv.DictReader(open(path))
@@ -11,10 +12,19 @@ class Client(object):
 				return True
 
 		return False
+		
+	def addClient(self,path):
+		with open(path, 'a', newline='') as csvfile:
+			spamwriter = csv.writer(csvfile, delimiter=',',quotechar=',', quoting=csv.QUOTE_MINIMAL)
+			spamwriter.writerow([self.nom]+[self.prenom]+[self.age])
+
+
 
 
 
 
 if __name__ == "__main__":
-	connexion = Client("tlemcani","aladin")
-	print(connexion.connect(path = "resources/clients.csv"))
+	connexion = Client("tlemcani","sa","16")
+	new_client = [{"nom" : ["tlemcani","bouazouni","mohand"],
+				   "prenom" : ["abdelhak","walid","mohamed"] }]
+	print(connexion.addClient(path = "resources/clients.csv"))
