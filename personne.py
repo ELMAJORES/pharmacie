eb1db6fa4f5a46a0c2a8f3c2948cfc97eae9208a
ELMAJORES <tlemcaniabdelhak2008@gmail.com>
2023-04-15 14:10:55 +0200

    create person class

---
+++ personne.py
@@ -0,0 +1,10 @@
+class personne():
+
+    def __init__(self,nom = "xx",prenom = "xx",date_naissance = "xx"):
+        self.nom = nom
+        self.prenom = prenom
+        self.date_naissance = date_naissance
+
+    
+if __name__ == "__main__":
+    print("yes")
