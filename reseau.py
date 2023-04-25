9661e9b7d5ddb293614aaf5d4cbd2938a76b5e60
ELMAJORES <tlemcaniabdelhak2008@gmail.com>
2023-04-25 19:15:18 +0200

    creation class reseau

---
+++ reseau.py
@@ -0,0 +1,32 @@
+class reseau(object):
+
+    def __init__(self,nomReseau = "xx",coutReseau=0.0,tauxRembourssement=0.0):
+        self.nomReseau = nomReseau
+        self.coutReseau = coutReseau
+        self.tauxRembourssement = tauxRembourssement
+
+class mastercard(reseau):
+
+    def __init__(self):
+        super().__init__("mastercard",0.5,1)
+    
+class visa(reseau):
+    
+    def __init__(self):
+        super().__init__("visa", 0.25,1)
+        self.paysRembourssement = {
+            "Espagne" : 0.10,
+            "Etats-Unis" : 0.20,
+            "Portugal" : 0.15,
+            "Royaume-Uni" : 0.25,
+            "Reste du monde" : 0.30
+        }
+        
+        
+        
+    def getrembourssement(self,pays):
+        return 1-self.paysRembourssement[pays]
+if __name__ == "__main__":
+    vis = visa()
+    print(vis.getrembourssement("Royaume-Uni"))
+
