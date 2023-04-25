0e366eb142528c2a04999943c66002848cee098f
ELMAJORES <tlemcaniabdelhak2008@gmail.com>
2023-04-25 18:21:43 +0200

    amelioration de la slass cbancaire

--- cbancaire.py
+++ cbancaire.py
@@ -1,46 +1,44 @@
 class cbancaire():
-
     cbs = []
 
-
-    def __init__(self,nom = "xx", proprietaire = None,num_carte = "xx",exp = "xx", cvc = "xx"):
+    def __init__(self, nom="xx", proprietaire=None, num_carte="xx", exp="xx", cvc="xx"):
         self.nom = nom
         self.proprietaire = proprietaire
         self.num_carte = num_carte
         self.exp = exp
         self.cvc = cvc
 
-
-    def isUniqueCarte(self,num_carte):
+    def isUniqueCarte(self, num_carte):
         if len(cbancaire.cbs) == 0:
             return True
         else:
             for carte in cbancaire.cbs:
                 if carte.num_carte == num_carte:
                     return False
-            return True 
-                
+            return True
+
+    def isValidCarte(self):
+        digits = [int(d) for d in str(self.num_carte) if d.isdigit()]
+        digits.reverse()
+        doubled_digits = [2 * d if i % 2 != 0 else d for i, d in enumerate(digits)]
+        subtracted_digits = [d - 9 if d >= 10 else d for d in doubled_digits]
+        total = sum(subtracted_digits)
+
+        return total % 10 == 0
+    def payer(self, montant):
+        if not self.isValidCarte():
+            print("Impossible de traiter le paiement. La carte bancaire est invalide.")
+            return
+
+        if montant <= 0:
+            print("Le montant doit être supérieur à zéro.")
+            return
+
+
+        print(f"Paiement de {montant} € effectué avec succès avec la carte bancaire {self.num_carte}.")
 
 if __name__ == "__main__":
     pass
-"""    cb = cbancaire("xx","cx","cc","xx")
-    if cb.isUniqueCarte(cb.num_carte):
-        print("oui")
-        cbancaire.cbs.append(cb)
-    else:
-        print("non") 
-    
-    cb1 = cbancaire("xx","efzcafvd","csdhjgavj","xx")
-    if cb1.isUniqueCarte(cb1.num_carte):
-        print("oui")
-        cbancaire.cbs.append(cb1)
-    else:
-        print("non") 
-    
 
-    cb2 = cbancaire("xx","cx","cc","xx")
-    if cb2.isUniqueCarte(cb2.num_carte):
-        print("oui")
-        cbancaire.cbs.append(cb2)
-    else:
-        print("non") """
+
+
