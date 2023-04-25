a3ca23360f812454e73fe2b9cde6d5b75989c215
ELMAJORES <tlemcaniabdelhak2008@gmail.com>
2023-04-25 20:08:33 +0200

    amelioration des fonctionalité dans cbancaire et ajout de cbphamacie

--- cbancaire.py
+++ cbancaire.py
@@ -2,13 +2,15 @@ from reseau import *
 class cbancaire():
     cbs = []
 
-    def __init__(self,reseau, nom="xx", proprietaire=None, num_carte="xx", exp="xx", cvc="xx"):
+    def __init__(self,reseau, nom="xx", proprietaire=None, num_carte="xx", exp="xx", cvc="xx",solde = 40.2):
         self.nom = nom
         self.proprietaire = proprietaire
         self.num_carte = num_carte
         self.exp = exp
         self.cvc = cvc
         self.reseau = reseau
+        self.solde = solde
+    
 
     def isUniqueCarte(self, num_carte):
         if len(cbancaire.cbs) == 0:
@@ -37,14 +39,38 @@ class cbancaire():
             print("Le montant doit être supérieur à zéro.")
             return
 
-
+        self.solde -= montant
         print(f"Paiement de {montant} € effectué avec succès avec la carte bancaire {self.num_carte}.")
 
     def remboursser(self,pays,prix):
-        return prix*self.reseau.getrembourssement(pays)
-    
+        self.solde += prix*self.reseau.getrembourssement(pays)
+
+
+class cbpharma(cbancaire):
+
+    def __init__(self, reseau, nom="xx", proprietaire=None, num_carte="xx", exp="xx", cvc="xx",solde = 50.0, portefeuilles = 0.0):
+        super().__init__(reseau, nom, proprietaire, num_carte, exp, cvc, solde)
+        self.portefeuilles = portefeuilles
+
+        
+    def setPorteFeuille(self,montant):    
+        self.portefeuilles += montant
+
+
+    def payerFinMois(self):
+        self.solde -= self.portefeuilles 
+        self.portefeuilles = 0
+
+
 if __name__ == "__main__":
     cb = cbancaire( reseau = mastercard(),nom = "lcl",proprietaire = "eee",num_carte = '123121321123',exp = '12', cvc="13")
-    print(cb.remboursser("Espagne",15))
+    cb2 = cbpharma( reseau = mastercard(),nom = "lcl",proprietaire = "eee",num_carte = '123121321123',exp = '12', cvc="13")
+
+    cb2.setPorteFeuille(15)
+    print(cb2.solde)
+    print(cb2.portefeuilles)
+    cb2.payerFinMois()
+    print(cb2.solde)
+    print(cb2.portefeuilles)
 
 
