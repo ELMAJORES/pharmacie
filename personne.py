361abf8d65a9f60aa416b3acffcd4dbdf0f0a992
ELMAJORES <tlemcaniabdelhak2008@gmail.com>
2023-04-25 20:40:14 +0200

    ajout de fonctionalité dans la classe personne

--- personne.py
+++ personne.py
@@ -1,10 +1,22 @@
+from cbancaire import * 
 class personne():
 
-    def __init__(self,nom = "xx",prenom = "xx",date_naissance = "xx"):
+    def __init__(self,carteBancaire1 ,carteBancaire2,nom = "xx",prenom = "xx",date_naissance = "xx"):
         self.nom = nom
         self.prenom = prenom
         self.date_naissance = date_naissance
+        self.carteBancaire1 = carteBancaire1
+        self.carteBancaire2 = carteBancaire2
 
+    def payer(self,montant):
+        print(self.carteBancaire2.payer(montant))
+    def contesterPayment():
+        return True
+    
 
 if __name__ == "__main__":
-    print("yes")
+    
+    pers = personne( carteBancaire1=cbancaire(visa()),carteBancaire2=cbpharma(mastercard()),nom = "tlemcani",prenom = "abdelhak",date_naissance="1207")
+    pers.payer(5)
+    print(pers.carteBancaire2.solde)
+    print(pers.carteBancaire2.portefeuilles)
