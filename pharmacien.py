9b4c48dea8ee6835d209de4a68ce1589548aadf5
ELMAJORES <tlemcaniabdelhak2008@gmail.com>
2023-04-26 13:35:46 +0200

    ajout de la classe pharmacie et classe produits

--- pharmacien.py
+++
@@ -1,9 +0,0 @@
-class Pharmacien(Employe):
-    def __init__(self, nom, prenom, adresse, salaire_fixe):
-        super().__init__(nom, prenom, adresse, salaire_fixe)
-
-    def calculer_prime(self, chiffre_affaires):
-        return chiffre_affaires * 0.01
-
-if __name__ == "__main__":
-    pass
